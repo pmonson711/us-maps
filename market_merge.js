ed19bb8682e8de11acf6dcbda012a8d15d5fd899
Paul Monson <pmonson@echo.com>
2014-07-01 17:35:41 -0500

    Adding market merge

---
+++ market_merge.js
@@ -0,0 +1,48 @@
+var turf = require("turf"),
+    _ = require("underscore"),
+    fs   = require("fs"),
+    base = 'geojson',
+    args = process.argv.splice(2),
+    market = _.last(args),
+    threeZips = _.initial(args);
+
+(function() {
+    var polygons = [];
+    for(var i in threeZips) {
+        var name = threeZips[i] + "";
+        if (name.length == 1) {
+            name = "00" + name;
+        }
+        if (name.length == 2) {
+            name = "0" + name;
+        }
+        console.log('Testing ' + name);
+        if (fs.existsSync(base+'/'+name+'.geojson')) {
+            console.log(' - found file for '+base + '/' + name + '.geojson');
+            var g = fs.readFileSync(base+'/'+name+'.geojson');
+            var polygon;
+            try {
+                polygon = JSON.parse(g);
+                polygons.push(polygon);
+            } catch(e) {
+                console.log("BAD JSON: " + name);
+            }
+        }
+    }
+
+    var f = {
+        type: "FeatureCollection",
+        features: _.flatten(_.pluck(polygons, "features"))
+    };
+    console.log(f);
+    var merged = turf.merge(f);
+    console.log(' - merged ' + name);
+    merged.properties.GEOID10 = market;
+    merged.properties.ZCTA5CE10 = market;
+    fs.writeFileSync(base+'/'+market+'.json', JSON.stringify(merged));
+
+    console.log(' - wrote ' + name);
+    console.log('');
+    console.log('==========');
+    console.log('');
+})();
